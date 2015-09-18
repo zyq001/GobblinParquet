46cb4f5d6cc14126f7e0cfde655367b0a737ff56
zangyq <zangyq@rd.netease.com>
2015-09-18 17:23:10 +0800

    build success

--- gobblin-core/src/main/java/gobblin/writer/ParquetHdfsDataWriter.java
+++ gobblin-core/src/main/java/gobblin/writer/ParquetHdfsDataWriter.java
@@ -1,10 +1,33 @@
 package gobblin.writer;
 
 import gobblin.source.extractor.schema.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.parquet.example.data.Group;
+
+import java.io.IOException;
 
 /**
  * Created by zangyq on 2015/9/13.
  */
-public class ParquetHdfsDataWriter extends DataWriterBuilder<Schema, Group>{
+class ParquetHdfsDataWriter extends FsDataWriter<Group>{
 
+
+    public ParquetHdfsDataWriter(){
+//        super();
+    }
+
+    @Override
+    public void write(Group record) throws IOException {
+
+    }
+
+    @Override
+    public long recordsWritten() {
+        return 0;
+    }
+
+    @Override
+    public long bytesWritten() throws IOException {
+        return 0;
+    }
 }
--- gobblin-core/src/main/java/gobblin/writer/ParquetHdfsDataWriterBuilder.java
+++ gobblin-core/src/main/java/gobblin/writer/ParquetHdfsDataWriterBuilder.java
@@ -1,7 +1,16 @@
 package gobblin.writer;
 
+import gobblin.source.extractor.schema.Schema;
+import org.apache.parquet.example.data.Group;
+
+import java.io.IOException;
+
 /**
  * Created by zangyq on 2015/9/13.
  */
-public class ParquetHdfsDataWriterBuilder {
+public class ParquetHdfsDataWriterBuilder extends DataWriterBuilder<Schema, Group>{
+    @Override
+    public DataWriter<Group> build() throws IOException {
+        return null;
+    }
 }
